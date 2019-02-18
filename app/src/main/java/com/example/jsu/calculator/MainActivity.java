7930a37d4ecfc8b21b361553a9a06e8bd2ecc64a
themerovingian <kfoster8@stu.jsu.edu>
2019-02-18 01:27:52 -0600

    version two - have sqrt, percent, and sign to finish

--- app/src/main/java/com/example/jsu/calculator/MainActivity.java
+++ app/src/main/java/com/example/jsu/calculator/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.jsu.calculator;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.lang.Math;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDecimal, btnDivision, btnMultiplication, btnSubtraction, btnAddition,
+           btnClear, btnSquare, btnPercent, btnEquals, btnSigned;
+
+    TextView showValue;
+    BigDecimal inputOne;
+    BigDecimal inputTwo;
+    boolean add, sub, mul, div, dec;
+    DecimalFormat decimal;
+    boolean decimalPoint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +36,194 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        showValue = findViewById(R.id.inputOutput);
+        decimal = new DecimalFormat("#.#######################");
 
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnDecimal = findViewById(R.id.btnDecimal);
+        btnDivision = findViewById(R.id.btnDivision);
+        btnMultiplication = findViewById(R.id.btnMultiplication);
+        btnSubtraction = findViewById(R.id.btnSubtraction);
+        btnAddition = findViewById(R.id.btnAddition);
+        btnClear = findViewById(R.id.btnClear);
+        btnSquare = findViewById(R.id.btnSquare);
+        btnPercent = findViewById(R.id.btnPercent);
+        btnEquals = findViewById(R.id.btnEquals);
+        btnSigned = findViewById(R.id.btnSigned);
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnDecimal.setOnClickListener(this);
+        btnDivision.setOnClickListener(this);
+        btnMultiplication.setOnClickListener(this);
+        btnSubtraction.setOnClickListener(this);
+        btnAddition.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnSquare.setOnClickListener(this);
+        btnPercent.setOnClickListener(this);
+        btnEquals.setOnClickListener(this);
+        btnSigned.setOnClickListener(this);
     }
 
+    @SuppressLint("SetTextI18n")
     public void onClick(View view){
 
-    }
+        decimalPoint = false;
+
+        switch(view.getId()){
+
+            case R.id.btn0:
+               showValue.append(btn0.getText() + "");
+               break;
+
+            case R.id.btn1:
+               showValue.append(btn1.getText() + "");
+               break;
+
+            case R.id.btn2:
+                showValue.append(btn2.getText() + "");
+                break;
+
+            case R.id.btn3:
+                showValue.append(btn3.getText() + "");
+                break;
+
+            case R.id.btn4:
+                showValue.append(btn4.getText() + "");
+                break;
+
+            case R.id.btn5:
+                showValue.append(btn5.getText() + "");
+                break;
+
+            case R.id.btn6:
+                showValue.append(btn6.getText() + "");
+                break;
+
+            case R.id.btn7:
+                showValue.append(btn7.getText() + "");
+                break;
+
+            case R.id.btn8:
+                showValue.append(btn8.getText() + "");
+                break;
+
+            case R.id.btn9:
+                showValue.append(btn9.getText() + "");
+                break;
+
+            case R.id.btnDecimal:
+                if(dec) {
+
+                }
+                else{
+                    showValue.append(btnDecimal.getText() + "");
+                    dec = true;
+                }
+                break;
+
+            case R.id.btnDivision:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                div = true;
+                dec = false;
+                showValue.setText(null);
+                break;
+
+            case R.id.btnMultiplication:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                mul = true;
+                dec = false;
+                showValue.setText(null);
+                break;
+
+            case R.id.btnSubtraction:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                sub = true;
+                dec = false;
+                showValue.setText(null);
+                break;
+
+            case R.id.btnAddition:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                add = true;
+                dec = false;
+                showValue.setText(null);
+                break;
+
+            case R.id.btnClear:
+                showValue.setText(null);
+                dec = false;
+                break;
+
+            case R.id.btnSquare:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                Double sqrSolution = (Math.sqrt(inputOne.doubleValue()));
+                showValue.setText(decimal.format(sqrSolution));
+                dec = false;
+                break;
+
+             case R.id.btnPercent:
+                inputOne = new BigDecimal(showValue.getText() + "");
+                showValue.setText(decimal.format(inputOne.doubleValue() / 100));
+                dec = false;
+                break;
+
+            case R.id.btnEquals:
+                inputTwo = new BigDecimal(showValue.getText() + "");
+                dec = false;
+
+                if(add){
+                    double addSolution = (inputOne.doubleValue() + inputTwo.doubleValue());
+                    showValue.setText(decimal.format(addSolution));
+                    add = false;
+                }
+
+                if(sub){
+                    double subSolution = (inputOne.doubleValue() - inputTwo.doubleValue());
+                    showValue.setText(decimal.format(subSolution));
+                    sub = false;
+                }
+
+                if(mul){
+                    double mulSolution = (inputOne.doubleValue() * inputTwo.doubleValue());
+                    showValue.setText(decimal.format(mulSolution));
+                    mul = false;
+                }
+
+                if(div){
+                    double divSolution = (inputOne.doubleValue()/inputTwo.doubleValue());
+                    showValue.setText(decimal.format(divSolution));
+                    div = false;
+                }
+
+                break;
+
+            case R.id.btnSigned:
+                showValue.append(btnSigned.getText() + "");
+                break;
+
+            }
+
+
+        }
 
 
     @Override
